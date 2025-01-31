7606a6ce16675d6b8f55d23e249b6ba5890331bc
Jace Yang <jaceyang8@gmail.com>
2025-01-31 23:50:07 +0800

    fix: minor translations

--- js/translations.js
+++ js/translations.js
@@ -14,10 +14,10 @@ const translations = {
     zh: {
         title: "杨泽群",
         name: "杨泽群",
-        quote: "人生就是一次梯度下降",
+        quote: "\"人生就是一次梯度下降\"",
         about: "[关于]",
         position: "量化开发工程师 @银河德睿",
-        education: "计算机科学和应用数学专业 @布兰戴斯 21'",
+        education: "计算机科学和应用数学专业 @布兰戴斯21届",
         connect: "[联系]",
         platform: "所有平台 @jaceyang97",
         blog: "[文章]",
